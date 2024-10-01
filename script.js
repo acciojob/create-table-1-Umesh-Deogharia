28250abf6979108519b2611ec75d9f90bf9a9996
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-10-01 09:12:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ function insert_Row() {
     //Write your code here
 let insert = document.getElementById("button");
 let table = document.getElementById("sampleTable");
-	let row = document.createElement("tr");
+	let row = table.insertRow(0);
+	// let row = document.createElement("tr");
 	let td1 = document.createElement("td");
 	td1.innerText = "New Cell1";
 	let td2 = document.createElement("td");
 	td2.innerText = "New Cell2";
 	row.appendChild(td1);
 	row.appendChild(td2);
-	table.appendChild(row);
+	// table.appendChild(row);
 }
